a5549c485d6a98a7b2e30cf771d9c4bad87ff76b
Luís Cipriani <cunhaluisg5@gmail.com>
2024-04-24 19:02:13 -0300

    Cria projeto

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app theme="light">
+    <v-main>
+      <v-container>
+        <h1>Página de teste</h1>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
